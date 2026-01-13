7a1a2e3f82a2b72f635ddbcb53f29c841b187d5e
Kyle Rhoads <kylerhoads45@gmail.com>
2026-01-12 19:35:45 -0800

    auto tile placement is working but still rough

--- Assets/Scripts/AutoTilePlacementTool.cs
+++ Assets/Scripts/AutoTilePlacementTool.cs
@@ -9,6 +9,7 @@ using Random = UnityEngine.Random;
 public class AutoTilePlacementTool : MonoBehaviour {
 
     public Tilemap tilemap;
+    public TileBase failedTile;
     public WaveFunctionCollapseTileRuleset ruleset;
     private Dictionary<Vector3Int, TileSuperPosition> superPositionLookup = new();
 
@@ -23,7 +24,6 @@ public class AutoTilePlacementTool : MonoBehaviour {
         superPositionLookup.Clear();
         
         BoundsInt dims = tilemap.cellBounds;
-        Debug.Log(dims.size);
 
         List<TileSuperPosition> superPositions = new();
 
@@ -33,7 +33,7 @@ public class AutoTilePlacementTool : MonoBehaviour {
         // Initialize all the superpositions
         
         for (int x = dims.xMin - 1; x < dims.xMax + 1; x++) {
-            for (int y = dims.yMin + 1; y < dims.yMax - 1; y++) {
+            for (int y = dims.yMin - 1; y < dims.yMax + 1; y++) {
                 Vector3Int pos = new(x, y, 0);
                 TileBase tile = tilemap.GetTile(pos);
                 GUID tileId = tile ? AssetDatabase.GUIDFromAssetPath(AssetDatabase.GetAssetPath(tile)) : new();
@@ -42,25 +42,35 @@ public class AutoTilePlacementTool : MonoBehaviour {
                     cellPosition = pos,
                     states = tileId == new GUID() ? new() { new() } : new(allTileIds),
                 };
+                
+                if (tile) {
+                    superPosition.states.Remove(new());
+                }
 
                 superPositions.Add(superPosition);
                 superPositionLookup.Add(pos, superPosition);  
             }
         }
         
+        bool retry = false;
         int maxIterations = 10000;
         int curIteration = 0;
         while (!WaveHasCollapsed(superPositions) && curIteration < maxIterations) {
             curIteration++;
             
             TileSuperPosition collapsingTile = GetLowestEntropy(superPositions);
+            
             GUID randomID = collapsingTile.states[Random.Range(0, collapsingTile.states.Count)];
             collapsingTile.states = new() { randomID };
             collapsingTile.collapsed = true;
             
-            UpdateNeighbors(collapsingTile);
+            bool res = UpdateNeighbors(collapsingTile);
+            
+            if (!res) {
+                tilemap.SetTile(collapsingTile.cellPosition, failedTile);
+                return;
+            }
         }
-        
         
         for (int x = dims.xMin; x < dims.xMax; x++) {
             for (int y = dims.yMin; y < dims.yMax; y++) {
@@ -101,7 +111,7 @@ public class AutoTilePlacementTool : MonoBehaviour {
         return lowest;
     }
 
-    private void UpdateNeighbors(TileSuperPosition collapsedTile) {
+    private bool UpdateNeighbors(TileSuperPosition collapsedTile) {
         Queue<TileSuperPosition> queue = new();
         queue.Enqueue(collapsedTile);
 
@@ -113,16 +123,23 @@ public class AutoTilePlacementTool : MonoBehaviour {
             Vector3Int westPos = new(cell.cellPosition.x - 1, cell.cellPosition.y, 0);
             Vector3Int eastPos = new(cell.cellPosition.x + 1, cell.cellPosition.y, 0);
             List<Vector3Int> neighborPositions = new() { northPos, southPos, westPos, eastPos, };
-
+            
             for (int i = 0; i < neighborPositions.Count; i++) {
                 Vector3Int nPos = neighborPositions[i];
                 if (superPositionLookup.TryGetValue(nPos, out TileSuperPosition neighborTile)) {
+                    
                     if (CheckToReducePossibleStates(cell, neighborTile, (Direction)i)) {
+                        
+                        if (neighborTile.states.Count <= 0) {
+                            return false;     
+                        }
+                        
                         queue.Enqueue(neighborTile);
                     }
                 }
             }
         } 
+        return true;
     }
 
     private enum Direction { North, South, West, East }
@@ -153,7 +170,7 @@ public class AutoTilePlacementTool : MonoBehaviour {
             if (!allowed.Contains(updatingTile.states[i]))
                 updatingTile.states.RemoveAt(i);
         }
-
+        
         return prevStateCount != updatingTile.states.Count;
     }
 
